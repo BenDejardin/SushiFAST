2d6a89007f216645262958a2fd410ecc2e110ed6
Ben Dejardin <bendejardin8@gmail.com>
2022-02-16 22:27:32 +0100

    Ajout de la partie code

---
+++ app/classes/ligne-commande.ts
@@ -0,0 +1,8 @@
+export class LigneCommande {
+    constructor(
+        public numCommande: number,
+        public nomPlateau: string,
+        public quantite: number,
+        public prix: number
+    ) { }
+}
